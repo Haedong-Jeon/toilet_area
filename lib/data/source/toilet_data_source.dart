fde27f6bd1c152541cb25a90f882f2e163fb2214
Haedong-Jeon <haedongjeon@icloud.com>
2022-10-29 21:38:16 +0900

    [data/source] getToiletData 구현

--- lib/data/source/toilet_data_source.dart
+++ lib/data/source/toilet_data_source.dart
@@ -2,8 +2,16 @@ import 'package:dio/dio.dart';
 import 'package:toilet_area/data/source/keys.dart';
 
 class ToiletDataSource {
- final String _dataEndpoint = "http://api.data.go.kr/openapi/tn_pubr_public_toilet_api";
- Future getToiletData() async {
-  return await Dio().get(_dataEndpoint,options: Options(headers: {"Authorization":toiletApiKey}));
- }
+  final String _dataEndpoint =
+      "http://api.data.go.kr/openapi/tn_pubr_public_toilet_api";
+
+  Future<Response> getToiletData() async {
+    final response = await Dio().get(_dataEndpoint, queryParameters: {
+      "serviceKey": toiletApiKey,
+      "type": "json",
+      "pageNo": 1,
+      "numOfRows": 100,
+    });
+    return response;
+  }
 }
