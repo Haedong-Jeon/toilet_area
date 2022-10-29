5cf1ce4b668147249bf6457807999be368dbb026
Haedong-Jeon <haedongjeon@icloud.com>
2022-10-29 21:09:43 +0900

    [domain/repository] toilet_data_respository 추상 클래스 추가

---
+++ lib/data/repository/toilet_data_repository_impl.dart
@@ -0,0 +1,12 @@
+import 'package:toilet_area/data/source/toilet_data_source.dart';
+import 'package:toilet_area/domain/repository/toilet_data_repository.dart';
+
+class ToiletDataRepositoryImpl extends ToiletDataRepository {
+  final ToiletDataSource dataSource;
+  ToiletDataRepositoryImpl(this.dataSource);
+
+  @override
+  Future getToiletData() async {
+    return await dataSource.getToiletData();
+  }
+}
