2c1b84a70c2a7bf5eca3c5280c4c5a9edd987cd7
Haedong-Jeon <haedongjeon@icloud.com>
2022-10-30 01:38:20 +0900

    [test] 사용자 생성 테스트 코드 추가

--- test/data/source/toilet_data_source_test.dart
+++ test/data/source/toilet_data_source_test.dart
@@ -1,11 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:toilet_area/data/repository/toilet_data_repository_impl.dart';
-import 'package:toilet_area/data/source/toilet_data_source.dart';
-import 'package:toilet_area/domain/use_case/get_toilet_list_use_case.dart';
+import 'package:toilet_area/data/repository/toilet/toilet_data_repository_impl.dart';
+import 'package:toilet_area/data/source/toilet/toilet_data_source.dart';
+import 'package:toilet_area/domain/model/user/user.dart';
+import 'package:toilet_area/domain/use_case/toilet/get_toilet_list_use_case.dart';
 
 void main() {
-  GetToiletListUseCase getToiletListUseCase = GetToiletListUseCase(ToiletDataRepositoryImpl(ToiletDataSource()));
   test("화장실 목록 받아오기", () async {
+    GetToiletListUseCase getToiletListUseCase = GetToiletListUseCase(ToiletDataRepositoryImpl(ToiletDataSource()));
     final response = await getToiletListUseCase();
     expect(response.data["response"]["header"]["resultCode"], "0");
   });
