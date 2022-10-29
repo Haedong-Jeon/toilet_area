acfffc5ca54e491ebbb72556bc21e928b54b10ca
Haedong-Jeon <haedongjeon@icloud.com>
2022-10-30 08:43:03 +0900

    [data/repository] 화장실 데이터를 불러오는 source, 원격/로컬 구분

--- lib/data/repository/toilet/toilet_data_repository_impl.dart
+++ lib/data/repository/toilet/toilet_data_repository_impl.dart
@@ -1,5 +1,5 @@
 import 'package:dio/dio.dart';
-import 'package:toilet_area/data/source/toilet/toilet_data_source.dart';
+import 'package:toilet_area/data/source/toilet/remote/toilet_data_source.dart';
 import 'package:toilet_area/domain/repository/toilet/toilet_data_repository.dart';
 
 class ToiletDataRepositoryImpl extends ToiletDataRepository {
