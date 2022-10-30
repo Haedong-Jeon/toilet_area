7fe17c850432f5c110eead75b36b6e13a5e31093
Haedong-Jeon <haedongjeon@icloud.com>
2022-10-30 14:25:14 +0900

    [presantation/toilet_list] 뷰모델에 ui이벤트 추가

--- lib/presentation/toilet_list/toilet_list_view_model.dart
+++ lib/presentation/toilet_list/toilet_list_view_model.dart
@@ -1,15 +1,21 @@
+import 'dart:async';
+
 import 'package:dio/dio.dart';
 import 'package:riverpod/riverpod.dart';
 import 'package:toilet_area/domain/model/toilet/toilet.dart';
 import 'package:toilet_area/domain/use_case/toilet/get_toilet_list_local_use_case.dart';
 import 'package:toilet_area/domain/use_case/toilet/get_toilet_list_remote_use_case.dart';
 import 'package:toilet_area/domain/use_case/toilet/save_toilet_list_use_case.dart';
+import 'package:toilet_area/presentation/toilet_list/toilet_list_ui_event.dart';
 
 class ToiletListViewModel extends StateNotifier<List<Toilet>> {
   final GetToiletListLocalUseCase getToiletListLocalUseCase;
   final GetToiletListFromRemoteUseCase getToiletListFromRemoteUseCase;
 
   final SaveToiletListUseCase saveToiletListUseCase;
+  final _uiEventController = StreamController<ToiletListUiEvent>.broadcast();
+
+  Stream<ToiletListUiEvent> get uiEventStream => _uiEventController.stream;
 
   ToiletListViewModel(this.getToiletListLocalUseCase,
       this.getToiletListFromRemoteUseCase, this.saveToiletListUseCase)
@@ -19,13 +25,20 @@ class ToiletListViewModel extends StateNotifier<List<Toilet>> {
 
   ///원격으로 화장실 목록을 불러오는 함수. 공공 API 응답에 따라 리팩토링 필요
   Future<List<Toilet>> getToiletListFromRemote() async {
-    Response response = await getToiletListFromRemoteUseCase(toiletListPage);
-    state = [...state, response.data["data"]["toilet_list"]];
-    saveToiletList(state);
+    _uiEventController.add(const ToiletListUiEvent.onLoading());
+    try {
+      Response response = await getToiletListFromRemoteUseCase(toiletListPage);
+      state = [...state, response.data["data"]["toilet_list"]];
+      _uiEventController.add(const ToiletListUiEvent.onSuccess());
+      saveToiletList(state);
+    } catch (e) {
+      _uiEventController.add(const ToiletListUiEvent.onError());
+    }
     return state;
   }
+
   Future<List<Toilet>> loadMoreToiletFromRemote() async {
-    toiletListPage +=1;
+    toiletListPage += 1;
     return await getToiletListFromRemote();
   }
 
