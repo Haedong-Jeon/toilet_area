e2b7030142b4380e74416bd2de48e34ee50e184e
Haedong-Jeon <haedongjeon@icloud.com>
2022-10-30 09:01:32 +0900

    [data/repository] getToiletListFromLocal(), saveToiletList() 추가

--- lib/data/repository/toilet/toilet_data_repository_impl.dart
+++ lib/data/repository/toilet/toilet_data_repository_impl.dart
@@ -1,13 +1,25 @@
 import 'package:dio/dio.dart';
-import 'package:toilet_area/data/source/toilet/remote/toilet_data_source.dart';
+import 'package:toilet_area/data/source/toilet/remote/toilet_data_remote_source.dart';
 import 'package:toilet_area/domain/repository/toilet/toilet_data_repository.dart';
 
 class ToiletDataRepositoryImpl extends ToiletDataRepository {
-  final ToiletDataSource dataSource;
-  ToiletDataRepositoryImpl(this.dataSource);
+  final ToiletDataRemoteSource remoteDataSource;
+  ToiletDataRepositoryImpl(this.remoteDataSource);
 
   @override
   Future<Response> getToiletListFromRemote() async {
-    return await dataSource.getToiletListFromRemote();
+    return await remoteDataSource.getToiletListFromRemote();
+  }
+
+  @override
+  Future getToiletListFromLocal() async {
+    // TODO: implement getToiletListFromLocal
+    throw UnimplementedError();
+  }
+
+  @override
+  Future saveToiletList() async {
+    // TODO: implement saveToiletList
+    throw UnimplementedError();
   }
 }
