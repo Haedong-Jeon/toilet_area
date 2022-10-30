7c74308145908a8c77eb5c911d0aa3c4baf94fa5
Haedong-Jeon <haedongjeon@icloud.com>
2022-10-30 10:13:29 +0900

    [presantation] 뷰모델을 ChangeNotifier → StateNotifier 변경

--- lib/presentation/toilet_list/toilet_list_view_model.dart
+++ lib/presentation/toilet_list/toilet_list_view_model.dart
@@ -1,5 +1,15 @@
-import 'package:flutter/material.dart';
+import 'package:riverpod/riverpod.dart';
+import 'package:toilet_area/domain/model/toilet/toilet.dart';
+import 'package:toilet_area/domain/use_case/toilet/get_toilet_list_local_use_case.dart';
+import 'package:toilet_area/domain/use_case/toilet/get_toilet_list_remote_use_case.dart';
+import 'package:toilet_area/domain/use_case/toilet/save_toilet_list_use_case.dart';
 
-class ToiletListViewModel extends ChangeNotifier {
+class ToiletListViewModel extends StateNotifier<List<Toilet>>{
+  final GetToiletListLocalUseCase getToiletListLocalUseCase;
+  final GetToiletListFromRemoteUseCase getToiletListFromRemoteUseCase;
 
+  final SaveToiletListUseCase saveToiletListUseCase;
+
+  ToiletListViewModel(this.getToiletListLocalUseCase,
+      this.getToiletListFromRemoteUseCase, this.saveToiletListUseCase) : super([]);
 }
