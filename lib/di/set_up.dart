96b395594a08c15172f5434e377351ce9399ee8c
Haedong-Jeon <haedongjeon@icloud.com>
2022-10-30 23:47:05 +0900

    [di] 카카오키 가져오는 usecase 추가

--- lib/di/set_up.dart
+++ lib/di/set_up.dart
@@ -5,6 +5,7 @@ import 'package:toilet_area/data/source/toilet/local/toilet_db_helper.dart';
 import 'package:toilet_area/data/source/toilet/remote/toilet_data_remote_source.dart';
 import 'package:toilet_area/domain/model/toilet/toilet.dart';
 import 'package:toilet_area/domain/repository/toilet/toilet_data_repository.dart';
+import 'package:toilet_area/domain/use_case/toilet/get_kakao_key_use_case.dart';
 import 'package:toilet_area/domain/use_case/toilet/get_toilet_list_local_use_case.dart';
 import 'package:toilet_area/domain/use_case/toilet/get_toilet_list_remote_use_case.dart';
 import 'package:toilet_area/domain/use_case/toilet/save_toilet_list_use_case.dart';
@@ -39,10 +40,13 @@ Future setUpProviders() async {
       GetToiletListFromRemoteUseCase(toiletDataRepository);
   SaveToiletListUseCase saveToiletListUseCase =
       SaveToiletListUseCase(toiletDataRepository);
+  GetKakaoKeyUseCase getKakaoKeyUseCase =
+      GetKakaoKeyUseCase(toiletDataRepository);
   ToiletListViewModel _toiletListViewModel = ToiletListViewModel(
     getToiletListLocalUseCase,
     getToiletListFromRemoteUseCase,
     saveToiletListUseCase,
+    getKakaoKeyUseCase,
   );
   TextViewModel _textViewModel = TextViewModel(TextControl());
   toiletListViewModelProvider =
