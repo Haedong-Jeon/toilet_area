3540a3564f708c638a19e86d627aecd08d60ad75
Haedong-Jeon <haedongjeon@icloud.com>
2022-10-31 01:35:39 +0900

    [presentation] 카카오 지도에서 구글 지도로 변경

--- lib/presentation/toilet_list/widget/map_widget.dart
+++ lib/presentation/toilet_list/widget/map_widget.dart
@@ -1,8 +1,14 @@
+import 'dart:async';
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:kakaomap_webview/kakaomap_webview.dart';
 import 'package:toilet_area/di/set_up.dart';
 import 'package:toilet_area/presentation/toilet_list/view_model/toilet_list_view_model.dart';
+import 'package:toilet_area/presentation/user/view_model/user_view_model.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapWidget extends ConsumerStatefulWidget {
   const MapWidget({Key? key}) : super(key: key);
@@ -12,21 +18,45 @@ class MapWidget extends ConsumerStatefulWidget {
 }
 
 class _MapWidgetState extends ConsumerState<MapWidget> {
+  Completer<GoogleMapController> _controller = Completer();
+  GoogleMapController? googleMapController;
+  double userZoom = 16;
+
+  void setMapController() async {
+    googleMapController = await _controller.future;
+  }
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    ToiletListViewModel toiletListViewModel = ref.watch(toiletListViewModelProvider.notifier);
-    return  KakaoMapView(
-        width: size.width,
-        height:size.height-300,
-        kakaoMapKey: toiletListViewModel.getKakaoKey(),
-        lat: 33.450701,
-        lng: 126.570667,
-        showMapTypeControl:false,
-        showZoomControl: false,
-        markerImageURL: 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png',
-        onTapMarker: (message) {
-          //event callback when the marker is tapped
+    ToiletListViewModel toiletListViewModel =
+        ref.watch(toiletListViewModelProvider.notifier);
+    UserViewModel userViewModel = ref.watch(userViewModelProvider.notifier);
+    setMapController();
+    return StreamBuilder<Position?>(
+        stream: userViewModel.getPositionStream(),
+        builder: (context, snapshot) {
+          double latitude = (snapshot.data?.latitude ?? -1);
+          double longitude = (snapshot.data?.longitude ?? -1);
+
+          return SizedBox(
+            width: size.width,
+            height: size.height - 300,
+            child: GoogleMap(
+              myLocationEnabled: true,
+              zoomControlsEnabled: true,
+              zoomGesturesEnabled: true,
+              onMapCreated: (mapController) {
+                _controller.complete(mapController);
+              },
+              initialCameraPosition: CameraPosition(
+                target: LatLng(
+                  snapshot.data?.latitude ?? 0,
+                  snapshot.data?.longitude ?? 0,
+                ),
+              ),
+            ),
+          );
         });
   }
 }
