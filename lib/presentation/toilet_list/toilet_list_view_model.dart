90ea5d958373efc82d56b284cbb8ceac0cfac0d3
Haedong-Jeon <haedongjeon@icloud.com>
2022-10-30 11:37:56 +0900

    [presantation] 뷰모델 내 함수 작성

--- lib/presentation/toilet_list/toilet_list_view_model.dart
+++ lib/presentation/toilet_list/toilet_list_view_model.dart
@@ -1,15 +1,35 @@
+import 'package:dio/dio.dart';
 import 'package:riverpod/riverpod.dart';
 import 'package:toilet_area/domain/model/toilet/toilet.dart';
 import 'package:toilet_area/domain/use_case/toilet/get_toilet_list_local_use_case.dart';
 import 'package:toilet_area/domain/use_case/toilet/get_toilet_list_remote_use_case.dart';
 import 'package:toilet_area/domain/use_case/toilet/save_toilet_list_use_case.dart';
 
-class ToiletListViewModel extends StateNotifier<List<Toilet>>{
+class ToiletListViewModel extends StateNotifier<List<Toilet>> {
   final GetToiletListLocalUseCase getToiletListLocalUseCase;
   final GetToiletListFromRemoteUseCase getToiletListFromRemoteUseCase;
 
   final SaveToiletListUseCase saveToiletListUseCase;
 
   ToiletListViewModel(this.getToiletListLocalUseCase,
-      this.getToiletListFromRemoteUseCase, this.saveToiletListUseCase) : super([]);
+      this.getToiletListFromRemoteUseCase, this.saveToiletListUseCase)
+      : super([]);
+
+  ///원격으로 화장실 목록을 불러오는 함수. 공공 API 응답에 따라 리팩토링 필요
+  Future<List<Toilet>> getToiletListFromRemote() async {
+    Response response = await getToiletListFromRemoteUseCase();
+    state = response.data["data"]["toilet_list"];
+    return state;
+  }
+
+  Future<List<Toilet>> getToiletListLocal() async {
+    List<Toilet> toilets = await getToiletListLocalUseCase();
+    state = toilets;
+    return state;
+  }
+
+  Future saveToiletList(List<Toilet> toilets) async {
+    await saveToiletListUseCase(toilets);
+    await getToiletListLocal();
+  }
 }
