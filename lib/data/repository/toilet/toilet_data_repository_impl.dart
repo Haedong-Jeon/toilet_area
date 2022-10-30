bb96532c01bd0d6d1db8791782928066223a7286
Haedong-Jeon <haedongjeon@icloud.com>
2022-10-30 14:25:35 +0900

    [data/repository] 예외 처리 추가

--- lib/data/repository/toilet/toilet_data_repository_impl.dart
+++ lib/data/repository/toilet/toilet_data_repository_impl.dart
@@ -5,30 +5,38 @@ import 'package:toilet_area/domain/model/toilet/toilet.dart';
 import 'package:toilet_area/domain/repository/toilet/toilet_data_repository.dart';
 
 class ToiletDataRepositoryImpl extends ToiletDataRepository {
-   ToiletDataRemoteSource? remoteDataSource;
-   ToiletDbHelper? toiletDbHelper;
-  ToiletDataRepositoryImpl(this.remoteDataSource, this.toiletDbHelper);
+  ToiletDataRemoteSource? remoteDataSource;
+  ToiletDbHelper? toiletDbHelper;
 
+  ToiletDataRepositoryImpl(this.remoteDataSource, this.toiletDbHelper);
 
   @override
   Future<Response?> getToiletListFromRemote(int page) async {
-    if(remoteDataSource == null) {
+    if (remoteDataSource == null) {
       return null;
     }
-    return await remoteDataSource!.getToiletListFromRemote(page);
+    try {
+      final response = await remoteDataSource!.getToiletListFromRemote(page);
+    } catch (e) {
+      rethrow;
+    }
   }
 
   @override
   Future getToiletListFromLocal() async {
-    if(toiletDbHelper == null) {
+    if (toiletDbHelper == null) {
       return;
     }
-    return await toiletDbHelper!.getToiletListFromLocal();
+    try {
+      return await toiletDbHelper!.getToiletListFromLocal();
+    } catch (e) {
+      rethrow;
+    }
   }
 
   @override
   Future saveToiletList(List<Toilet> toiletsFromRemote) async {
-    if(toiletDbHelper == null) {
+    if (toiletDbHelper == null) {
       return;
     }
     return await toiletDbHelper!.saveToiletList(toiletsFromRemote);
