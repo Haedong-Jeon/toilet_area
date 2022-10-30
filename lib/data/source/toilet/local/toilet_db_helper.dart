cdc8d573f2f3cce3cc2657c5d1c84d444d741a89
Haedong-Jeon <haedongjeon@icloud.com>
2022-10-30 10:11:46 +0900

    [data/source] 원격에서 받아온 화장실 목록을 로컬 DB에 저장 할 수 있도록 수정

--- lib/data/source/toilet/local/toilet_db_helper.dart
+++ lib/data/source/toilet/local/toilet_db_helper.dart
@@ -9,7 +9,26 @@ class ToiletDbHelper {
   // Future getToiletListFromLocal();
   // Future saveToiletList();
 
-  Future saveToiletList(List<Toilet> toilets) async {
+  Future saveToiletList(List<Toilet> toiletsFromRemote) async {
+    ///화장실 목록을 로컬 DB에 저장 하는데, 이미 있는 화장실일 경우 업데이트. 새로운 화장실일 경우 저장.
+    List<Toilet> localToiletList = await getToiletListFromLocal();
+    for (Toilet element in toiletsFromRemote) {
+      for (Toilet toilet in localToiletList) {
+        if (toilet.longitude == element.longitude && toilet.latitude == element.latitude) {
+          await db.update('toilet', element.toJson(),
+              where: "latitude = ? AND longitude = ?",
+              whereArgs: [element.latitude, element.longitude]);
+          break;
+        } else {
+          await db.insert('toilet', element.toJson());
+          break;
+        }
+      }
+    }
+  }
 
+  Future<List<Toilet>> getToiletListFromLocal() async {
+    final List<Map<String, dynamic>> maps = await db.query("toilet");
+    return maps.map((e) => Toilet.fromJson(e)).toList();
   }
 }
