e6b5c6b9c7bf7e2298f36f8a057d4c82e982e81d
Haedong-Jeon <haedongjeon@icloud.com>
2022-10-31 01:37:07 +0900

    [data/source]에 위치를 가져오는 소스 추가

---
+++ lib/data/source/user/geolocator.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+
+import 'package:geolocator/geolocator.dart';
+
+class GeoLocator {
+  Future getPermission() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+  }
+
+  Future getUserPosition() async {
+    await Geolocator.getCurrentPosition();
+  }
+
+  Stream<Position> getPositionStream() {
+    return Geolocator.getPositionStream();
+  }
+}
