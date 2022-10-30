343bd8305b708e2ada8c9e418cc9d1784f7bbe0b
Haedong-Jeon <haedongjeon@icloud.com>
2022-10-30 10:12:04 +0900

    [domain/repository] saveToiletList에 인자 전달

--- lib/domain/repository/toilet/toilet_data_repository.dart
+++ lib/domain/repository/toilet/toilet_data_repository.dart
@@ -1,5 +1,7 @@
+import 'package:toilet_area/domain/model/toilet/toilet.dart';
+
 abstract class ToiletDataRepository {
   Future getToiletListFromRemote();
   Future getToiletListFromLocal();
-  Future saveToiletList();
+  Future saveToiletList(List<Toilet> toiletsFromRemote);
 }
