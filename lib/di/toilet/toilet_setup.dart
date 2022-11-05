6d9bcc6820b7cab72afbdcd027bad019129951ba
Haedong-Jeon <haedongjeon@icloud.com>
2022-11-05 18:00:55 +0900

    [di] 가까운 화장실 찾기

--- lib/di/toilet/toilet_setup.dart
+++ lib/di/toilet/toilet_setup.dart
@@ -6,8 +6,10 @@ import 'package:toilet_area/data/source/toilet/remote/toilet_data_remote_source.
 import 'package:toilet_area/domain/model/toilet/toilet.dart';
 import 'package:toilet_area/domain/repository/toilet/toilet_data_repository.dart';
 import 'package:toilet_area/domain/use_case/toilet/get_kakao_key_use_case.dart';
+import 'package:toilet_area/domain/use_case/toilet/get_nearest_toilet_pos_use_case.dart';
 import 'package:toilet_area/domain/use_case/toilet/get_toilet_list_local_use_case.dart';
 import 'package:toilet_area/domain/use_case/toilet/get_toilet_list_remote_use_case.dart';
+import 'package:toilet_area/domain/use_case/toilet/is_no_toilet_nearby_use_case.dart';
 import 'package:toilet_area/domain/use_case/toilet/save_toilet_list_use_case.dart';
 import 'package:toilet_area/presentation/toilet_list/view_model/toilet_list_view_model.dart';
 
@@ -36,13 +38,18 @@ Future setUp() async {
       GetToiletListFromRemoteUseCase(toiletDataRepository);
   SaveToiletListUseCase saveToiletListUseCase =
       SaveToiletListUseCase(toiletDataRepository);
+  IsNoToiletNearByUseCase isNoToiletNearByUseCase = IsNoToiletNearByUseCase();
+  GetNearestToiletPosUseCase getNearestToiletPosUseCase =
+      GetNearestToiletPosUseCase();
   GetKakaoKeyUseCase getKakaoKeyUseCase =
       GetKakaoKeyUseCase(toiletDataRepository);
 
   ToiletListViewModel _toiletListViewModel = ToiletListViewModel(
     getToiletListLocalUseCase,
     getToiletListFromRemoteUseCase,
+    getNearestToiletPosUseCase,
     saveToiletListUseCase,
+    isNoToiletNearByUseCase,
     getKakaoKeyUseCase,
   );
   toiletListViewModelProvider =
